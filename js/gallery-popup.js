67d306564399ebf8a8f7c7f15ebe32340105f235
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-08-29 22:15:31 +0300

    добавил реализацию модалки и кнопок

--- js/gallery-popup.js
+++ js/gallery-popup.js
@@ -1,17 +1,26 @@
 import popupImage from './gallery.js';
-const galleryEl = document.querySelector('.js-gallery');
-const closeBtn = document.querySelector('[data-action="close-lightbox"]');
+const refs = {
+    galleryEl: document.querySelector('.js-gallery'),
+    closeBtn: document.querySelector('[data-action="close-lightbox"]'),
+    lightbox: document.querySelector('.js-lightbox'),
+    overlay: document.querySelector('.js-overlay'),
+    ['lightbox-img']: document.querySelector('.lightbox__image'),
+};
 
-galleryEl.addEventListener('click', onOpenPopup);
-closeBtn.addEventListener('click', onClosePopup);
+refs.galleryEl.addEventListener('click', onOpenPopup);
+refs.closeBtn.addEventListener('click', onClosePopup);
+refs.overlay.addEventListener('click', onBackdropClick);
 
 function onOpenPopup(event) {
     event.preventDefault();
 
+    window.addEventListener('keydown', onEscKeyPress);
+    window.addEventListener('keydown', onKeyPressChangingImg);
+
     const IMG = 'img';
 
     if (event.target.localName === IMG) {
-        document.querySelector('.js-lightbox').classList.add('is-open');
+        refs.lightbox.classList.add('is-open');
     }
 
     openImageInPopup(event.target.dataset.source);
@@ -20,14 +29,54 @@ function onOpenPopup(event) {
 function openImageInPopup(source) {
     return popupImage.filter(({ original, description }) => {
         if (original === source) {
-            document.querySelector('.lightbox__image').src = original;
-            document.querySelector('.lightbox__image').alt = description;
+            refs['lightbox-img'].src = original;
+            refs['lightbox-img'].alt = description;
         }
     });
 }
 
 function onClosePopup() {
-    document.querySelector('.js-lightbox').classList.remove('is-open');
-    document.querySelector('.lightbox__image').removeAttribute('src');
-    document.querySelector('.lightbox__image').removeAttribute('alt');
+    window.removeEventListener('keydown', onEscKeyPress);
+    window.removeEventListener('keydown', onKeyPressChangingImg);
+
+    refs.lightbox.classList.remove('is-open');
+    refs['lightbox-img'].removeAttribute('src');
+    refs['lightbox-img'].removeAttribute('alt');
+}
+
+function onEscKeyPress(event) {
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+        onClosePopup();
+    }
+}
+
+function onBackdropClick(event) {
+    if (event.target === event.currentTarget) {
+        onClosePopup();
+    }
+}
+
+function onKeyPressChangingImg(event) {
+    const ARROW_KEY_CODE = {
+        ARROW_LEFT: 'ArrowLeft',
+        ARROW_RIGHT: 'ArrowRight',
+    };
+
+    switch (event.code) {
+        case ARROW_KEY_CODE.ARROW_RIGHT:
+            showNextSlide(ARROW_KEY_CODE.ARROW_RIGHT);
+            break;
+        case ARROW_KEY_CODE.ARROW_LEFT:
+            showPrevSlide(ARROW_KEY_CODE.ARROW_LEFT);
+            break;
+    }
+}
+
+function showNextSlide(right) {
+    return console.log(right);
+}
+
+function showPrevSlide(left) {
+    return console.log(left);
 }
